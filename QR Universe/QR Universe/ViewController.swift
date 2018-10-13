ce278f24a9acfab454751f68a948174d1fbd5834
Viraat <viraat.laldas@gmail.com>
2018-10-12 22:41:27 -0400

    First Commit

---
+++ QR Universe/QR Universe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QR Universe
+//
+//  Created by Viraat Das on 10/12/18.
+//  Copyright © 2018 Viraat Das. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

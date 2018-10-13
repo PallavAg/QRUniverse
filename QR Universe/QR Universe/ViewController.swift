74025172d55dff0d60c76c4e1cc946e64009d109
Viraat <viraat.laldas@gmail.com>
2018-10-13 02:48:12 -0400

    camera implem

--- QR Universe/QR Universe/ViewController.swift
+++ QR Universe/QR Universe/ViewController.swift
@@ -1,37 +1,45 @@
-//
-//  ViewController.swift
-//  QR Universe
-//
-//  Created by Viraat Das on 10/12/18.
-//  Copyright © 2018 Viraat Das. All rights reserved.
-//
-
 import UIKit
-//import Firebase
 
-
-class ViewController: UIViewController, UIImagePickerControllerDelegate,
-UINavigationControllerDelegate{
-
-    @IBOutlet weak var imagePicked: UIImageView!
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate  {
+    
+    @IBOutlet weak var imageTake: UIImageView!
+    
+    var imagePicker: UIImagePickerController!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-
     }
     
-    @IBAction func openCameraButton(_ sender: Any) {
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            var imagePicker = UIImagePickerController()
-            imagePicker.delegate = self
-            imagePicker.sourceType = .camera;
-            imagePicker.allowsEditing = false
-            self.present(imagePicker, animated: true, completion: nil)
+    //MARK: - Take image
+    @IBAction func takePhoto(_ sender: UIButton) {
+        imagePicker =  UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    //MARK: - Saving Image here
+    @IBAction func save(_ sender: AnyObject) {
+        UIImageWriteToSavedPhotosAlbum(imageTake.image!, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+    }
+    
+    //MARK: - Add image to Library
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            // we got back an error!
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
         }
-        
     }
     
-
-
+    //MARK: - Done image capture here
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imagePicker.dismiss(animated: true, completion: nil)
+        imageTake.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+    }
+    
 }
-
